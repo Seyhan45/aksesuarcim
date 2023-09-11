f42bdbe844a1588e9f0cb5a251b8fd51698e9c17
seyhan yandık <se_y_han@hotmail.com>
2023-09-11 21:27:37 +0300

    Add project files.

---
+++ aksesuarcim/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using aksesuarcim.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace aksesuarcim.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult Orta_Alan()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
